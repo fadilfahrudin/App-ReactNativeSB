12f90697113dd6add828b0ecb37d9c90331da456
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-07-03 17:37:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {SignIn, SplashScreen} from './pages';
 import {NavigationContainer} from '@react-navigation/native';
-import Router from './rooter';
+import Router from './Router';
 
 const App = () => {
   return (
