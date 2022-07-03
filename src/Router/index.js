86a01aa99a3dad60b0d5c6fb77ebea23f0063b7a
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-07-03 17:36:14 +0700

    Add router

---
+++ src/Router/index.js
@@ -0,0 +1,58 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {
+  SignIn,
+  SignUp,
+  SplashScreen,
+  Home,
+  DonasiSaya,
+  Profile,
+} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {BottomNavigator} from '../components';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Donasi Saya" component={DonasiSaya} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+const Router = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SplashScreen" //option untuk menyembunyikan halaman yang saat ini
+        component={SplashScreen} //ambil dari component Splash Screen
+        options={{headerShown: false}} //option untuk menyembunyikan halaman yang saat ini
+      />
+      <Stack.Screen
+        name="SignIn" //option untuk menyembunyikan halaman yang saat ini
+        component={SignIn} //ambil dari component SignIn
+        options={{headerShown: false}} //option untuk menyembunyikan halaman yang saat ini
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
