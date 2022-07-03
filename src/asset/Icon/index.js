85f10767ba08192b4193a01ec3db0e32b69e496c
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-07-03 17:37:46 +0700

    Update index assets

--- src/asset/Icon/index.js
+++ src/asset/Icon/index.js
@@ -1,2 +1,6 @@
 import IcBack from './ic-Back.svg';
-export {IcBack};
+import IcCeklist from './ic-ceklist.svg';
+import IcForward from './ic-forward.svg';
+import IcLike from './ic-like.svg';
+import IcSearch from './ic-search.svg';
+export {IcBack, IcForward, IcCeklist, IcLike, IcSearch};
--- src/asset/Illustration/index.js
+++ src/asset/Illustration/index.js
@@ -1,3 +1,19 @@
 import Logo from './Logo.svg';
+import LogoHeader from './logo-header.svg';
+import BtProfile from './bt-profile.svg';
+import BtProfileActive from './bt-profileActive.svg';
+import BtMyDonateActive from './bt-myDonateActive.svg';
+import BtMyDonate from './bt-myDonate.svg';
+import BtDonateActive from './bt-donationActive.svg';
+import BtDonate from './bt-donation.svg';
 
-export { Logo };
+export {
+  Logo,
+  LogoHeader,
+  BtProfile,
+  BtProfileActive,
+  BtMyDonateActive,
+  BtMyDonate,
+  BtDonateActive,
+  BtDonate,
+};
