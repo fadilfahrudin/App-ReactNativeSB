adce03584eee493c350a073f129f9dd72c9e56a9
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-07-04 08:29:14 +0700

    update moleculs index

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,4 +1,5 @@
 import Header from './Header';
 import BottomNavigator from './BottomNavigator';
+import ProgramCard from './ProgramCard';
 
-export {Header, BottomNavigator};
+export {Header, BottomNavigator, ProgramCard};
