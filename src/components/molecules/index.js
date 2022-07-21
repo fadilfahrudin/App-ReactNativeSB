21d4b30e91cada9a94d8e8b3b7edd1b7b5832091
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-07-21 19:02:47 +0700

    [add]infoCard, HomeTabSection, HomeProfile,TabProg

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,17 @@
 import Header from './Header';
 import BottomNavigator from './BottomNavigator';
 import ProgramCard from './ProgramCard';
+import InfoCard from './InfoCard';
+import HomeTabSection from './HomeTabSection';
+import HomeProfile from './HomeProfile';
+import TabProgram from './TabProgram';
 
-export {Header, BottomNavigator, ProgramCard};
+export {
+  Header,
+  BottomNavigator,
+  ProgramCard,
+  InfoCard,
+  HomeTabSection,
+  HomeProfile,
+  TabProgram,
+};
