b678d8c26fc1a38b75b3cf8b0c8d3edad719bd62
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-09-13 00:29:18 +0700

    [update] storage dan showmessage utils

--- src/utils/showMessage/index.js
+++ src/utils/showMessage/index.js
@@ -7,6 +7,7 @@ export const showMessage = (message, type) => {
   showToast({
     message,
     type: type === 'success' ? 'success' : 'danger',
+    duration: 5000,
     // backgroundColor: type === 'success' ? 'hijau' : 'merah'
   });
 };
--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -1,19 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {showMessage} from '../showMessage';
 
-export const storeData = async value => {
+export const storeData = async (storageKey, value) => {
   try {
     const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem('@storage_Key', jsonValue);
+    await AsyncStorage.setItem(storageKey, jsonValue);
   } catch (e) {
     // saving error
+    showMessage('Gagal menyimpan di local storage');
   }
 };
 
-export const getData = async () => {
+export const getData = async storageKey => {
   try {
-    const jsonValue = await AsyncStorage.getItem('@storage_Key');
+    const jsonValue = await AsyncStorage.getItem(storageKey);
     return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (e) {
     // error reading value
+    showMessage('Gagal mengambil data dari local storage');
   }
 };
